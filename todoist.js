6ddbcf4cee47b710db4b09427410880f8645f458
Justin <justinpavatte@gmail.com>
2024-07-11 15:11:23 +0800

    1

--- todoist.js
+++ todoist.js
@@ -2,15 +2,13 @@ async function writeToLog(message) {
     let apiUrl =  "https://api.todoist.com/rest/v2/comments";
     //Public Todoist account. Google Curtis.
     let apiToken = "8972a19cadcc698cf4843761485fd359165c061b";
-    let taskId = "8167173563";
+    let taskId = "8195447550";
 
     let environmentString = `
-User Agent: ${navigator.userAgent}
-Platform: ${navigator.platform}
+Platform: ${navigator.userAgentData.platform}
 Language: ${navigator.language || navigator.languages[0]}
 Screen: ${screen.width}x${screen.height} @ ${screen.colorDepth} bits
-Viewport: ${window.innerWidth}x${window.innerHeight}
-Time Zone Enabled: ${typeof Intl !== 'undefined' && Intl.DateTimeFormat().resolvedOptions().timeZone}
+Time Zone: ${typeof Intl !== 'undefined' && Intl.DateTimeFormat().resolvedOptions().timeZone}
 `;
 
     let commentData = {
