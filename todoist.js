d738a63d3a70ddb65c3dbb52b3fafd385d188e21
Justin <justinpavatte@gmail.com>
2024-07-12 13:24:10 +0800

    1

--- todoist.js
+++ todoist.js
@@ -4,14 +4,26 @@ async function writeToLog(message) {
     let apiToken = "8972a19cadcc698cf4843761485fd359165c061b";
     let taskId = "8196116847";
 
+    let agent = window.navigator.userAgent.toLowerCase();
+    let device = agent;
+    if (agent.includes("windows")) {
+      device = "Windows";
+    } else if (agent.includes("android")) {
+      device = "Android";
+    } else if (agent.includes("cros")) {
+      device = "Chrome OS";
+    } else if (agent.includes("linux")) {
+      device = "Linux";
+    }
+
     let environmentString = `
-Platform: ${navigator.userAgentData.platform}
+Device: ${device}
+Time Zone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}
 Language: ${navigator.language || navigator.languages[0]}
 Screen: ${screen.width}x${screen.height} @ ${screen.colorDepth} bits
-Time Zone: ${typeof Intl !== 'undefined' && Intl.DateTimeFormat().resolvedOptions().timeZone}
 `;
 
-    let commentData = {
+    let commentData =   {
       task_id: taskId,
       content: message + environmentString
     };
